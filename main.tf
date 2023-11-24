5e799eee0d2ac79fa05fd33824f1aa5d78128c14
amrin4623 <z.amrin4623@gmail.com>
2023-11-24 15:53:39 +0530

    add zone in providers txt

--- main.tf
+++ main.tf
@@ -1,10 +1,10 @@
 resource "google_storage_bucket" "bucket" {
-  name     = "test-bucket-pavi-project-1234"
+  name     = "test-bucket-amrin-project-1234"
   location = "europe-west2"
 }
 
 resource "google_storage_bucket" "gcs_bucket" {
-  name     = "test-bucket-pavi-project-5678"
+  name     = "test-bucket-amrin-project-5678"
   location = "europe-west2"
 }
 
